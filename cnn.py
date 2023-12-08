90419327ed7af6d8ef4803cc088d9cd4c6778cd8
RASHIDA NK <119100586+rashidank@users.noreply.github.com>
2023-12-08 14:35:44 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -136,10 +136,6 @@ def make_prediction(img,model):
 print(make_prediction('Dataset_Celebrities/cropped/virat_kohli/virat_kohli10.png',model))
 print('--------------------------------------------------------')
 
-pd.DataFrame(history.history).plot(figsize=(8,5))
-plt.grid(True)
-plt.gca().set_ylim(0,1)
-plt.show()
 
 
 
